bfcd2c240cfb4b214d0e1158c18e67c801b332b8
sailybaevvv <sailybaevvv@gmail.com>
2025-05-22 21:21:41 +0500

    Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'NEWTONE - Детейлинг Студия',
+  description: 'Профессиональные услуги детейлинга, которые преображают ваш автомобиль с тщательным вниманием к деталям и премиальными продуктами.',
 }
 
 export default function RootLayout({
